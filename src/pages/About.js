fceef5ff02838ad9fdfd04389e56df4fefefe728
Shane Lin <sam6091260@gmail.com>
2023-03-22 16:33:29 +0800

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about" style={{ minHeight: "100vh" }}>
+      <h1>About</h1>
+      <p>
+        It's my REACT practise project. <br />
+        Create by SHANE
+      </p>
+    </div>
+  );
+};
+
+export default About;
